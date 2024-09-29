cea1def0e0c5caa8aa26c4946a584e41d82003cd
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-09-29 09:39:00 -0500

    Fixed the server a little bit

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const express = require('express');
 // Creating express object
 const app = express();
 
+app.use((req, res, next)=>{
+    if(req.path.startsWith("/.git")||req.path.startsWith("/node_modules"))return;
+    next()
+})
+app.get("/", (req, res)=>{
+    res.redirect("/Pages/Home.html")
+})
+app.use(express.static("./"))
+
 // Handling GET request
-app.get('/', (req, res) => { 
-    res.send('node is '
-        + 'noding') 
-    res.end() 
-}) 
+// app.get('/', (req, res) => { 
+//     res.send('node is '
+//         + 'noding') 
+//     res.end() 
+// }) 
 
 // Port Number
 const PORT = process.env.PORT ||5000;
