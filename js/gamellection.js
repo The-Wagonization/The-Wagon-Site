0aff1efaa7b438731e361a8911742360dabc3843
Jack Wagon <148831953+JackWagon885@users.noreply.github.com>
2024-10-21 18:58:19 -0400

    new games!!1!
    we got some new games
    3 Slices
    3 Slices 2
    Learn to Fly
    Learn to Fly 2
    Wordle+

--- js/gamellection.js
+++ js/gamellection.js
@@ -1,7 +1,12 @@
 function searchFunction() {
     var input = document.getElementById('searchInput').value.toLowerCase();
     var data = [
-        '<a href="..\Pages\Subpages\Gamellection Subpages\SmashKarts.html">Smash Karts</a>', '<a href="a">game</a>'
+        '<a href="..\Pages\Subpages\Gamellection Subpages\3Slices.html">3 Slices</a>',
+        '<a href="..\Pages\Subpages\Gamellection Subpages\3Slices2.html">3 Slices 2</a>',
+        '<a href="..\Pages\Subpages\Gamellection Subpages\LearnToFly.html">Learn to Fly</a>',
+        '<a href="..\Pages\Subpages\Gamellection Subpages\LearnToFly2.html">Learn to Fly 2</a>',
+        '<a href="..\Pages\Subpages\Gamellection Subpages\SmashKarts.html">Smash Karts</a>',
+        '<a href="..\Pages\Subpages\Gamellection Subpages\Wordle+.html">Wordle+</a>',
     ];
     var result = data.filter(item => item.toLowerCase().includes(input));
 
