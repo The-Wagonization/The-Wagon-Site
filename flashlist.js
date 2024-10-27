cf632803ff3106b5d358c0d7cc86cecc00ae55c7
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-10-27 13:32:21 -0500

    flashlist.js

--- flashlist.js
+++ flashlist.js
@@ -5,6 +5,7 @@ const flashHtml = path.resolve("./Pages/flash.html");
 const flashGamePathUnresolved = "/Pages/Subpages/Flash/Flash Games";
 const flashGamePath = path.resolve("." + flashGamePathUnresolved);
 
+console.log("Constructing list...")
 const flashGameList = JSON.stringify(
   fs
     .readdirSync(flashGamePath, { recursive: true })
@@ -12,6 +13,9 @@ const flashGameList = JSON.stringify(
     .map((e) => path.join(flashGamePathUnresolved, e).replaceAll("\\", "/"))
 );
 
+console.log("Applying list...")
 let html = fs.readFileSync(flashHtml, "utf-8");
 html = html.replace(/\[.*\](?=; \/\/ Game file list)/, flashGameList);
 fs.writeFileSync(flashHtml, html, "utf-8");
+
+console.log("Done updating flash game list!")
