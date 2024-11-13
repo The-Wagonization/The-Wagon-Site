edacc8741d765a58fbe6ca5f40202addb10758ab
Jack Wagon <148831953+JackWagon885@users.noreply.github.com>
2024-11-13 20:38:07 +0000

    Update gamellection.js

--- js/gamellection.js
+++ js/gamellection.js
@@ -3,7 +3,11 @@ function searchFunction() {
     var data = [
         '<a href="..\Pages\Subpages\Gamellection Subpages\3Slices.html">3 Slices</a>',
         '<a href="..\Pages\Subpages\Gamellection Subpages\3Slices2.html">3 Slices 2</a>',
+        '<a href="..\Pages\Subpages\Gamellection Subpages\BigFLAPPYTowerVSTinySquare.html">Big FLAPPY Tower VS Tiny Square</a>',
+        '<a href="..\Pages\Subpages\Gamellection Subpages\BigICETowerTinySquare.html">Big ICE Tower Tiny Square</a>',
+        '<a href="..\Pages\Subpages\Gamellection Subpages\BigNEONTowerVSTinySquare.html">Big NEON Tower VS Tiny Square</a>',
         '<a href="..\Pages\Subpages\Gamellection Subpages\BigTowerTinySquare.html">Big Tower Tiny Square</a>',
+        '<a href="..\Pages\Subpages\Gamellection Subpages\BigTowerTinySquare2.html">Big Tower Tiny Square 2</a>',
         '<a href="..\Pages\Subpages\Gamellection Subpages\BTD.html">Bloons Tower Defense</a>',
         '<a href="..\Pages\Subpages\Gamellection Subpages\BTD2.html">Bloons Tower Defense 2</a>',
         '<a href="..\Pages\Subpages\Gamellection Subpages\LearnToFly.html">Learn to Fly</a>',
