7e17acf80d53982dd72c47a34a160f0152310c90
Jack Wagon <148831953+JackWagon885@users.noreply.github.com>
2024-11-13 20:30:21 +0000

    Update gamellection.js

--- js/gamellection.js
+++ js/gamellection.js
@@ -3,6 +3,9 @@ function searchFunction() {
     var data = [
         '<a href="..\Pages\Subpages\Gamellection Subpages\3Slices.html">3 Slices</a>',
         '<a href="..\Pages\Subpages\Gamellection Subpages\3Slices2.html">3 Slices 2</a>',
+        '<a href="..\Pages\Subpages\Gamellection Subpages\BigTowerTinySquare.html">Big Tower Tiny Square</a>',
+        '<a href="..\Pages\Subpages\Gamellection Subpages\BTD.html">Bloons Tower Defense</a>',
+        '<a href="..\Pages\Subpages\Gamellection Subpages\BTD2.html">Bloons Tower Defense 2</a>',
         '<a href="..\Pages\Subpages\Gamellection Subpages\LearnToFly.html">Learn to Fly</a>',
         '<a href="..\Pages\Subpages\Gamellection Subpages\LearnToFly2.html">Learn to Fly 2</a>',
         '<a href="..\Pages\Subpages\Gamellection Subpages\SmashKarts.html">Smash Karts</a>',
