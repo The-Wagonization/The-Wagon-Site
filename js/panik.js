ba89ae90c437acc5390886d0e1c388f3ca9430ec
xz8f <163360403+crossjbly@users.noreply.github.com>
2026-01-20 00:30:04 -0700

    Change panic URL redirection to open in a new window

--- js/panik.js
+++ js/panik.js
@@ -2,6 +2,7 @@ document.addEventListener("keydown", function(event) {
     if (event.key === '`') {
         const PanicURL = localStorage.getItem("p") || "https://classroom.google.com/u/1/";
         console.log("yoinkers");
-        window.location.href = PanicURL;
+        window.open(PanicURL);
+        window.top.close();
     }
 });
